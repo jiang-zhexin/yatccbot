44d48397048216cc94a7eb1bf4b3c5b07d7cf394
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2025-02-20 19:48:56 +0800

    fix: gemini tool call error: only 'enum' and 'date-time' are supported for STRING type

--- src/tool/getWebsiteContent.ts
+++ src/tool/getWebsiteContent.ts
@@ -3,7 +3,7 @@ import { z } from "zod"
 
 export const getWebsiteContent = tool({
     description: "Extracting web content",
-    parameters: z.object({ url: z.string().url().describe("The url of Website") }),
+    parameters: z.object({ url: z.string().describe("The url of Website") }),
     execute: async (args, options) => {
         console.log({ args: args })
         return fetch(args.url).then((resp) => resp.text())
